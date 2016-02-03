5b3f366de8ffecdd90181e522f2c609519998074
Vincent Dorut <vincent.dorut@camptocamp.com>
2016-02-03 10:38:50 +0100

    user info

--- ldapadmin/src/main/webapp/console/app/app.js
+++ ldapadmin/src/main/webapp/console/app/app.js
@@ -2,16 +2,40 @@
 angular.module('admin_console', [
   'ngResource',
   'ngNewRouter',
-  'angular-chosen'
+  'angular-chosen',
+  'flash',
+  'pascalprecht.translate'
 ]).controller('AppController', [
   '$router', AppController
-]).constant('LDAP_BASE_URI', '/ldapadmin/private/');
+]).constant('LDAP_BASE_URI', '/ldapadmin/private/')
+.config(['$translateProvider', function ($translateProvider) {
+  $translateProvider
+  .translations('en', {
+    // TABS
+    'tab.infos'     : 'Infos',
+    'tab.groups'    : 'Groups',
+    'tab.analytics' : 'Analytics',
+    'tab.messages'  : 'Messages',
+    'tab.logs'      : 'Logs',
+    'tab.manage'    : 'Manage',
+    // USER
+    'group.system'    : 'System groups',
+    'group.app'       : 'Application groups',
+    'msg.date'        : 'Date',
+    'msg.subject'     : 'Subject',
+    'msg.sender'      : 'Sender',
+    'msg.messages'    : 'Messages',
+    'msg.on'          : 'on',
+    'msg.attachments' : 'Attachments',
+  }).preferredLanguage('en').useSanitizeValueStrategy('escape');
+}]);
 
 require('./components/users/users');
 require('./components/groups/groups');
 require('./components/user/user');
 require('./components/home/home');
 require('./services/groups');
+require('./services/groups_users');
 require('./services/users');
 
 function AppController($router) {
--- ldapadmin/src/main/webapp/console/app/components/user/user.js
+++ ldapadmin/src/main/webapp/console/app/components/user/user.js
@@ -1,20 +1,119 @@
 angular.module('admin_console')
 .controller('UserController', [
-  '$routeParams', 'User', 'Group', 'groupAdminList', 'groupAdminFilter',
+  '$routeParams', '$q',
+  'Flash', 'User', 'Group', 'Email',
+  'groupAdminList', 'groupAdminFilter',
   UserController
 ]);
 
-function UserController($routeParams, User, Group, groupAdminList, groupAdminFilter) {
-  this.user = User.get({id : $routeParams.id});
+function UserController($routeParams, $q, Flash, User, Group, Email,
+    groupAdminList, groupAdminFilter) {
   this.tab = $routeParams.tab;
+  this.user = User.get({id : $routeParams.id}, function() {
+    if (this.tab == 'messages') {
+      Email.query({id: this.user.uuid}, function(r) {
+        // this.messages = r.emails;
+        this.messages =  [{
+          "sender": "98192574-18d0-1035-8e10-c310a114ab8f",
+          "id": 51,
+          "body": "qsdfqsdfqsf",
+          "subject": "Hello",
+          "attachments": [
+            {
+              "id": 42,
+              "name": "intelij.jpeg",
+              "mimeType": "image/jpeg",
+              "size": 30218
+            },
+            {
+              "id": 43,
+              "name": "intelij.jpeg",
+              "mimeType": "image/jpeg",
+              "size": 30218
+            }
+          ],
+          "date": "2007-03-01T13:00:00Z",
+        "recipient": "9818af68-18d0-1035-8e0e-c310a114ab8f"},
+        {
+          "sender": "98192574-18d0-1035-8e10-c310a114ab8f",
+          "id": 52,
+          "body": "Hello ça va ?",
+          "subject": "Hi men :!",
+          "attachments": [{
+            "id": 44,
+            "name": "intelij.jpeg",
+            "mimeType": "image/jpeg",
+            "size": 30218
+          }],
+          "date": "2015-11-23T16:44:18.00Z",
+        "recipient": "9818af68-18d0-1035-8e0e-c310a114ab8f"}
+        ];
+      }.bind(this));
+    }
+  }.bind(this));
   this.tabs = [ 'infos', 'groups', 'analytics', 'messages', 'logs', 'manage' ];
   this.adminGroups = groupAdminList();
-  this.groups = Group.query(function(){
-    this.groups = this.groups.filter(function(g) {
-      return !groupAdminFilter(g);}
-    );
-  }.bind(this));
+  switch (this.tab) {
+    case 'groups':
+      var notAdmin = [];
+      this.groups = Group.query();
+      $q.all([
+        this.user.$promise,
+        this.groups.$promise
+      ]).then(function() {
+        this.user.groups = this.user.groups || [];
+        this.user.adminGroups = this.user.adminGroups || [];
+        this.groups.forEach(function(group) {
+          if (group.users.indexOf(this.user.uid)>=0) {
+            if (groupAdminFilter(group)) {
+              this.user.adminGroups.push(group.cn);
+            } else {
+              this.user.groups.push(group.cn);
+            }
+          }
+          if (!groupAdminFilter(group)) {
+            notAdmin.push(group.cn);
+          }
+        }.bind(this));
+        this.groups = notAdmin;
+      }.bind(this));
+      break;
+    default:
+  }
 }
+
+UserController.prototype.activate = [ '$scope', '$cacheFactory',
+    'LDAP_BASE_URI', 'GroupsUsers', 'Flash',
+    function($scope, $cacheFactory, baseUri, GroupsUsers, Flash) {
+  $scope.$watch(function(){
+    return this.user.groups;
+  }.bind(this), function(newVal, oldVal) {
+    if (!newVal || !oldVal) {
+      return;
+    }
+    var toAdd = newVal.filter(function(a) {
+      return oldVal.indexOf(a) == -1;
+    });
+    var toRemove = oldVal.filter(function(a) {
+      return newVal.indexOf(a) == -1;
+    });
+    if (toAdd.length == 0 && toRemove.length == 0) {
+      return;
+    }
+    var $httpDefaultCache = $cacheFactory.get('$http');
+    GroupsUsers.save({
+      users: [ this.user.uid ],
+      PUT: toAdd,
+      DELETE: toRemove
+    }, function() {
+      Flash.create('success', 'Groups updated');
+      // $httpDefaultCache.remove(baseUri + 'groups');
+      $httpDefaultCache.removeAll();
+    }, function() {
+      Flash.create('error', 'Error associating to groups');
+    });
+  }.bind(this));
+}];
 
 UserController.prototype.save = function() {
   this.user.$update(function(r) {
@@ -27,4 +126,11 @@ UserController.prototype.save = function() {
   }, function() {
     console.log('user error');
   });
+}
+
+UserController.prototype.openMessage = function(message) {
+  this.message = message;
+}
+UserController.prototype.closeMessage = function(message) {
+  delete this.message;
 }
